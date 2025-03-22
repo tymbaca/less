a1bd070b60febc69a041543681450ca23d0423cf
t.rostomyan <t.rostomyan@hldtech.ru>
2025-03-22 04:27:11 +0200

    draft

---
+++ election.go
@@ -0,0 +1,98 @@
+package lex
+
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// Implementation is responsible for internal (per call) retries
+type Storage interface {
+	Renew(ctx context.Context, key string, deadline time.Time) error
+	Get(ctx context.Context, key string) (string, error)
+	SetNX(ctx context.Context, key, val string, deadline time.Time) (bool, error)
+}
+
+type Candidate struct {
+	id       string
+	isLeader bool
+
+	storage Storage
+	key     string
+
+	ttl       time.Duration
+	applyRate time.Duration
+	renewRate time.Duration
+}
+
+func New(ctx context.Context, storage Storage) *Candidate {
+	cand := &Candidate{
+		id:       uuid.New().String(),
+		isLeader: false,
+
+		storage: storage,
+		key:     "leader",
+
+		ttl:       10 * time.Second,
+		applyRate: 2 * time.Second,
+		renewRate: 2 * time.Second,
+	}
+
+	go follow(ctx, cand)
+
+	return cand
+}
+
+func follow(ctx context.Context, cand *Candidate) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(cand.applyRate):
+		}
+
+		ok, err := cand.storage.SetNX(ctx, cand.key, cand.id, time.Now().Add(cand.ttl))
+		if err != nil {
+			slog.Error("can't setnx", "err", err)
+			continue
+		}
+
+		if ok {
+			slog.Info("we acquired leadership")
+			hold(ctx, cand)
+		}
+	}
+}
+
+func hold(ctx context.Context, cand *Candidate) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-time.After(cand.renewRate):
+		}
+
+		err := cand.storage.Renew(ctx, cand.key, time.Now().Add(cand.ttl))
+		if err != nil {
+			slog.Error("can't renew", "err", err)
+			continue
+		}
+
+		current, err := cand.storage.Get(ctx, cand.key)
+		if err != nil {
+			slog.Error("can't get", "err", err)
+			continue
+		}
+
+		if current != cand.id {
+			slog.Warn("we lost leadership")
+			return
+		}
+	}
+}
+
+func (c *Candidate) IsLeader() bool {
+	return c.isLeader
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tymbaca/lex
+
+go 1.22.5
+
+require github.com/google/uuid v1.6.0
